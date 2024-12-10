979b34385d5751358ca143d0c7d4b1efbff58fb2
Kab-35-16 <Kab-35-16@KAB-32-10->
2024-12-10 11:18:42 +0200

    3423

--- Program.cs
+++ Program.cs
@@ -10,19 +10,19 @@ namespace prac_11
     {
         static void Main(string[] args)
         {
-            
-            int[] num = { 1, 2, 3, 4, 5 };
-            int mult = 3;
-            for (int i = 0; i < numbers.Length; i++)
-            {
-              num[i] *= mult; 
-            }
-            Console.WriteLine("Массив после умножения на " + mult + ":");
-            foreach (int el in num)
-            {
-              Console.WriteLine(num);
-            }
-            Console.ReadLine();
+            //Задача 12.Умножение всех элементов массива на определенное число.
+            //int[] num = { 1, 2, 3, 4, 5 };
+            //int mult = 3;
+            //for (int i = 0; i < num.Length; i++)
+            //{
+            //  num[i] *= mult; 
+            //}
+            //Console.WriteLine($"Массив после умножения на { mult } ");
+            //foreach (int el in num)
+            //{
+            //  Console.WriteLine(el);
+            //}
+            //Console.ReadLine();
         }
     }
 }
