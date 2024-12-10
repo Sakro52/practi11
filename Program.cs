26f0323d877ca92fb7627ab6fb2ad6219aac9a9f
Kab-35-16 <Kab-35-16@KAB-32-10->
2024-12-10 10:46:52 +0200

    1245478

--- Program.cs
+++ Program.cs
@@ -10,13 +10,12 @@ namespace prac_11
     {
         static void Main(string[] args)
         {
-            //Задача 5.Реверс массива.
-            //int[] omas = { -1, 2, -3, 4, -5 };
-            //Array.Reverse(omas);
-            //foreach (int i in omas)
-            //{
-            //    Console.WriteLine(i);
-            //}
+            //Задача 6. Подсчет количества вхождений определенного числа в массив.
+            //int[] omas = { 1, 2, 3, 4, 5, 2, 2, 6, 7, 2 };
+            //Console.Write("Введите число для подсчета: ");
+            //int i = int.Parse(Console.ReadLine());
+            //int count = Array.FindAll(omas, x => x == i).Length;
+            //Console.WriteLine($"Число {i} встречается {count} раза в массиве.");
             //Console.ReadLine();
         }
     }
